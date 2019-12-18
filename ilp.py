2197d478aec6c6afb3244f67392babe2d48740d7
phil8192 <phil@parasec.net>
2019-12-18 12:09:31 +0000

    limits vector

--- ilp.py
+++ ilp.py
@@ -2,7 +2,10 @@ import pandas as pd
 from pulp import *
 
 data = pd.read_csv('../data/family_data.csv', index_col='family_id')
+lims = pd.read_csv('n.csv')
+lims = lims["x"].values
 
+sol = pd.read_csv('/tmp/lala.csv')
 
 fam_day_choices = {i:[] for i in range(0,5000)}
     
@@ -33,9 +36,10 @@ for i in range(0, 100):
 
 obj = []
 #choices = ['choice_0', 'choice_1', 'choice_2', 'choice_3', 'choice_4', 'choice_5', 'choice_6', 'choice_7', 'choice_8', 'choice_9']
-choices = ['choice_0', 'choice_1', 'choice_2', 'choice_3'] 
+choices = ['choice_0', 'choice_1', 'choice_2', 'choice_3']
         
 for j in range(0, 5000):
+    sol_choice = sol['assigned_day'][j]
     fam_choice = []
     for i in range(0, len(choices)):
         choice = choices[i]
@@ -43,7 +47,12 @@ for j in range(0, 5000):
         fam_day_choices[j].append(k)
         num_ppl = data['n_people'][j]
         penalty = cost(i, num_ppl)
-        ass = LpVariable('ASS_{}_{}_{}'.format(i, j, k), cat='Binary')
+        ass = LpVariable('ASS_{}_{}_{}'.format(i, j, k), lowBound=0, upBound=1, cat=LpBinary)
+        if k == sol_choice:
+            ass.setInitialValue(True)
+        else:
+            ass.setInitialValue(False)
+        
         obj.append(penalty * ass)
         fam_choice.append(ass)
         day_assignments[k - 1].append(num_ppl * ass)
@@ -64,14 +73,15 @@ for i in range(0, 5000):
 # each day must have between 125 and 300 ppl. 
 for i in range(0, 100):
     day_assignment = day_assignments[i]
-    santa += lpSum(day_assignment) <= 300, "DAY_CONSTRAINT_LT{}".format(i+1)
+    #santa += lpSum(day_assignment) <= 300, "DAY_CONSTRAINT_LT{}".format(i+1)
+    santa += lpSum(day_assignment) <= lims[i], "DAY_CONSTRAINT_LT{}".format(i+1)
     santa += lpSum(day_assignment) >= 125, "DAY_CONSTRAINT_GT{}".format(i+1)
 
 santa.writeLP('santa.lp')
 
 # using coin-or solver
 threads = 12 
-santa.solve(solver=COIN_CMD(msg=1, mip=1, presolve=1, strong=1, cuts=1, maxSeconds=60*300, threads=threads))
+santa.solve(solver=COIN_CMD(msg=1, mip=1, presolve=1, strong=0, cuts=1, maxSeconds=60*300, dual=0, threads=threads))
 # gnu
 #santa.solve(solver=GLPK_CMD(keepFiles=0, mip=1, msg=1))
 
@@ -90,7 +100,7 @@ with open('lala.csv', 'w') as f:
         x = fam_choices[i]
         ass_v = [choice.value() for choice in x]
         ass = fam_day_choices[i][ass_v.index(max(ass_v))]
-        #print(ass_v)
+        print(ass_v)
         f.write("{},{}\n".format(i,ass))
 
 
