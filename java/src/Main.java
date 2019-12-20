5929e4c66020357a938c3384e559fe6d00e5f0ef
phil8192 <phil@parasec.net>
2019-12-20 03:56:10 +0000

    start cost

--- java/src/Main.java
+++ java/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
 	private final double[][] penalties;
 	private final double[][] accounting;
 
+	//private final int[][] choices;
+
+
+	// all family penalties
+	// 5000*100
+	final double[][] all_penalties = new double[5000][100];
+
 	Main() {
 		this.penalties = new double[MAX_FAM_SIZE + 1][MAX_CHOICE];
 		for (int i = 1; i < MAX_FAM_SIZE; i++) {
@@ -31,6 +38,19 @@ public class Main {
 				accounting[i][j] = ((now - 125) / 400.0) * Math.pow(now, 0.5 + (Math.abs(now - pre) / 50.0));
 			}
 		}
+	}
+
+	// [fam_choice, fam_choice] ... 5000
+
+	private double cost(final int[] familyAssignments) {
+		double penalty = 0.0;
+		double accounting = 0.0;
+		for(int i = 0, len = familyAssignments.length; i++) {
+			final int day = familyAssignments[i];
+			penalty += all_penalties[i][day];
+
+		}
+		return penalty + accounting;
 	}
 
 	public static void main(String[] meh) {
