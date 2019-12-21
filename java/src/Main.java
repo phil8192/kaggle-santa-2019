8c060650f4c9c9bea6ea746e4928babd4b08bdd5
phil8192 <phil@parasec.net>
2019-12-21 11:51:07 +0000

    fix accounting cost

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs;
+
 public class Main {
 	private static final int MAX_FAM_SIZE = 10;
 	private static final int MAX_CHOICE = 10;
@@ -5,16 +7,15 @@ public class Main {
 	private static final int MAX_PPL = 300;
 
 	private final double[][] penalties;
-	private final double[][] accounting;
+	private final double[][] accountingCost;
 
-	//private final int[][] choices;
-
+	private final int[] dayCapacities;
 
 	// all family penalties
 	// 5000*100
-	final double[][] all_penalties;
+	final double[][] allPenalties;
 
-	Main(int[][] family_data) {
+	Main(int[][] familyData, int[] initialAssignments) {
 		this.penalties = new double[MAX_FAM_SIZE + 1][MAX_CHOICE+1];
 		for (int i = 1; i < MAX_FAM_SIZE; i++) {
 			penalties[i][0] = 0;
@@ -29,55 +30,74 @@ public class Main {
 			penalties[i][9] = 500 + 36 * i + 199 * i;
 			penalties[i][10] = 500 + 36 * i + 398 * i;
 		}
-		// init accounting matrix
-		int max_cap = MAX_PPL - MIN_PPL;
-		this.accounting = new double[max_cap][max_cap];
-		for (int i = 0; i < max_cap; i++) {
-			int now = MIN_PPL + i;
-			for (int j = 0; j < max_cap; j++) {
-				int pre = MIN_PPL + j;
-				accounting[i][j] = ((now - 125) / 400.0) * Math.pow(now, 0.5 + (Math.abs(now - pre) / 50.0));
-			}
-		}
 		// init all penalties matrix
-		all_penalties = new double[5000][100+1];
-		for(int i = 0; i < all_penalties.length; i++) {
-			for(int j = 0; j < all_penalties[0].length; j++) {
-				all_penalties[i][j] = Double.MAX_VALUE;
+		allPenalties = new double[5000][100+1];
+		for(int i = 0; i < allPenalties.length; i++) {
+			for(int j = 0; j < allPenalties[0].length; j++) {
+				allPenalties[i][j] = Double.MAX_VALUE;
 			}
 		}
-		for(int i = 0; i < family_data.length; i++) {
-			int famSize = family_data[i][11];
+		for(int i = 0; i < familyData.length; i++) {
+			int famSize = familyData[i][11];
 			for(int j = 0; j < 10; j++) {
-				int choice_j = family_data[i][j+1];
-				all_penalties[i][choice_j] = penalties[famSize][j];
+				int choice_j = familyData[i][j+1];
+				allPenalties[i][choice_j] = penalties[famSize][j];
 			}
 		}
+		// init accounting matrix
+		int max_cap = MAX_PPL+1; // 1 to 300 ppl
+		accountingCost = new double[max_cap][max_cap];
+		for (int i = 1; i < max_cap; i++) {
+			int now = i;
+			for (int j = 1; j < max_cap; j++) {
+				int pre = j;
+				accountingCost[i][j] = ((now - 125) / 400.0) * Math.pow(now, 0.5 + (abs(now - pre) / 50.0));
+			}
+		}
+		// init day capacities
+		dayCapacities = new int[100+1];
+		for(int i = 0; i < initialAssignments.length; i++) {
+			int day = initialAssignments[i];
+			int famSize = familyData[i][11];
+			dayCapacities[day]+=famSize;
+		}
+	}
+
+	private double getAccountingCost(final int now, final int pre) {
+		//int diff = abs(now - pre);
+		//return ((now - 125) / 400.0) * Math.pow(now, 0.5 + (diff / 50.0));
+		return accountingCost[now][pre];
 	}
 
 	private double cost(final int[] familyAssignments) {
 		double penalty = 0.0;
 		double accounting = 0.0;
+		// day assignment penalty
 		for(int i = 0, len = familyAssignments.length; i < len; i++) {
 			final int day = familyAssignments[i];
-			final double fam_penalty = all_penalties[i][day];
-			assert fam_penalty != Double.MAX_VALUE;
-			penalty += fam_penalty;
+			penalty += allPenalties[i][day];
+		}
+		// accounting penalty
+		// first day is special
+		accounting += getAccountingCost(dayCapacities[100], dayCapacities[100]);
+		// rest of days starting at day 99
+		for(int i = 100; --i > 0; ) {
+			accounting += getAccountingCost(dayCapacities[i], dayCapacities[i+1]);
 		}
 		return penalty + accounting;
 	}
 
 	public static void main(String[] meh) {
 		int[][] family_data = CsvReader.read("../../../data/family_data.csv");
-		//int[][] starting_solution = CsvReader.read("../../../submission_71647.5625.csv");
-		int[][] starting_solution = CsvReader.read("../../lala.csv");
-		int[] starting_assignments = new int[starting_solution.length];
+		int[][] starting_solution = CsvReader.read("../../../submission_71647.5625.csv");
+		//int[][] starting_solution = CsvReader.read("../../lala.csv");
+		int[] initialAsignments = new int[starting_solution.length];
 		for(int i = 0; i < starting_solution.length; i++) {
-			starting_assignments[i] = starting_solution[i][1];
+			initialAsignments[i] = starting_solution[i][1];
 		}
-		Main main = new Main(family_data);
+		Main main = new Main(family_data, initialAsignments);
 		long l = System.currentTimeMillis();
-		double cost = main.cost(starting_assignments);
+		double cost = main.cost(initialAsignments);
 		System.out.println(cost + " (" + (System.currentTimeMillis() - l) + "ms)");
 	}
 }
