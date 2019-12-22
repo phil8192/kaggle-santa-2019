bf3465d3063e8693b65e73aa0f47caf95880dd1e
phil8192 <phil@parasec.net>
2019-12-22 11:26:10 +0000

    fix day capacity bug

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 import static java.lang.Math.abs;
@@ -25,7 +26,7 @@ public class Main {
 	private Main(int[][] familyData, int[] initialAssignments) {
 		this.familyPrefs = new int[5000][10];
 		this.familySize = new int[5000];
-		this.penalties = new double[MAX_FAM_SIZE + 1][MAX_CHOICE+1];
+		this.penalties = new double[MAX_FAM_SIZE + 1][MAX_CHOICE + 1];
 		for (int i = 1; i < MAX_FAM_SIZE; i++) {
 			penalties[i][0] = 0;
 			penalties[i][1] = 50;
@@ -40,23 +41,23 @@ public class Main {
 			penalties[i][10] = 500 + 36 * i + 398 * i;
 		}
 		// init all penalties matrix
-		allPenalties = new double[5000][100+1];
-		for(int i = 0; i < allPenalties.length; i++) {
-			for(int j = 0; j < allPenalties[0].length; j++) {
+		allPenalties = new double[5000][100 + 1];
+		for (int i = 0; i < allPenalties.length; i++) {
+			for (int j = 0; j < allPenalties[0].length; j++) {
 				allPenalties[i][j] = Double.MAX_VALUE;
 			}
 		}
-		for(int i = 0; i < familyData.length; i++) {
+		for (int i = 0; i < familyData.length; i++) {
 			int famSize = familyData[i][11];
 			familySize[i] = famSize;
-			for(int j = 0; j < 10; j++) {
-				int choice_j = familyData[i][j+1];
+			for (int j = 0; j < 10; j++) {
+				int choice_j = familyData[i][j + 1];
 				familyPrefs[i][j] = choice_j;
 				allPenalties[i][choice_j] = penalties[famSize][j];
 			}
 		}
 		// init accounting matrix
-		int max_cap = MAX_PPL+1; // 1 to 300 ppl
+		int max_cap = MAX_PPL + 1; // 1 to 300 ppl
 		accountingCost = new double[max_cap][max_cap];
 		for (int i = 1; i < max_cap; i++) {
 			for (int j = 1; j < max_cap; j++) {
@@ -64,11 +65,11 @@ public class Main {
 			}
 		}
 		// init day capacities
-		dayCapacities = new int[100+1];
-		for(int i = 0; i < initialAssignments.length; i++) {
+		dayCapacities = new int[100 + 1];
+		for (int i = 0; i < initialAssignments.length; i++) {
 			int day = initialAssignments[i];
-			int famSize = familyData[i][11];
-			dayCapacities[day]+=famSize;
+			int famSize = familySize[i];
+			dayCapacities[day] += famSize;
 		}
 	}
 
@@ -84,7 +85,7 @@ public class Main {
 		double penalty = 0.0;
 		double accounting = 0.0;
 		// day assignment penalty
-		for(int i = 0, len = familyAssignments.length; i < len; i++) {
+		for (int i = 0, len = familyAssignments.length; i < len; i++) {
 			final int day = familyAssignments[i];
 			penalty += allPenalties[i][day];
 		}
@@ -92,14 +93,17 @@ public class Main {
 		// first day is special
 		accounting += getAccountingCost(dayCapacities[100], dayCapacities[100]);
 		// rest of days starting at day 99
-		for(int i = 100; --i > 0; ) {
-			accounting += getAccountingCost(dayCapacities[i], dayCapacities[i+1]);
+		for (int i = 100; --i > 0; ) {
+			accounting += getAccountingCost(dayCapacities[i], dayCapacities[i + 1]);
 		}
 		return penalty + accounting;
 	}
 
 	private double tryMove(final int assignedDay, final int candidateDay, final int famSize, final int fam,
 												 final int[] assignments, final double best, final double t) {
+//		if(assignments[fam] != assignedDay) {
+//			throw new RuntimeException("assigned day mismatch! " + assignments[fam] + " != " + assignedDay);
+//		}
 		// break and eval
 		dayCapacities[assignedDay] -= famSize;
 		dayCapacities[candidateDay] += famSize;
@@ -107,6 +111,8 @@ public class Main {
 		final double candidateScore = cost(assignments);
 
 		if (candidateScore < best || prng.nextDouble() < t) {
+//			System.out.println("tryMove famSize = " + famSize + " ass day = " + assignedDay + " can day = " + candidateDay + " best = " + best + " candidate score = " + candidateScore);
+//			sanity(assignments);
 			return candidateScore - best;
 		} else { // fix
 			dayCapacities[assignedDay] += famSize;
@@ -118,70 +124,91 @@ public class Main {
 
 	private double localMinima(final int[] assignments, final double t) {
 		double best = cost(assignments);
+		System.out.println(best);
 		boolean improvement;
 		do {
 			improvement = false;
-			fams: for (int i = 0; i < assignments.length; i++) { // each family i
+			for (int i = 0; i < assignments.length; i++) { // each family i
 				final int[] prefs = familyPrefs[i];
 				final int famSize = familySize[i];
 				final int assignedDay = assignments[i];
 				final int assignedDayCap = dayCapacities[assignedDay];
-				if (assignedDayCap - famSize < MIN_PPL) {
-					continue; // can not move this family out of current day (constraint violation)
-				}
-				for (int j = 0; j < 10; j++) {
-					final int candidateDay = prefs[j];
-					if (candidateDay != assignedDay) {
-						final int candidateDayCap = dayCapacities[candidateDay];
-						if (candidateDayCap + famSize > MAX_PPL) {
-							continue; // can not move this family to candidate day (constraint violation)
-						}
-						final double delta = tryMove(assignedDay, candidateDay, famSize, i, assignments, best, t);
-						if(delta < 0.0) {
-							best += delta;
-							improvement = true;
-							i = 0;
-							break fams; // start from family 0 again
+				if (assignedDayCap - famSize >= MIN_PPL) {
+					for (int j = 0; j < 10; j++) {
+						final int candidateDay = prefs[j];
+						if (candidateDay != assignedDay) {
+							final int candidateDayCap = dayCapacities[candidateDay];
+							if (candidateDayCap + famSize <= MAX_PPL) {
+								//System.out.println("ass day = " + assignedDay + " can day = " + candidateDay +" assigned day cap = " + assignedDayCap + " candidate day cap = " + candidateDayCap + " famSize = " + famSize);
+
+								final double delta = tryMove(assignedDay, candidateDay, famSize, i, assignments, best, t);
+
+								if (delta < 0.0) {
+//									System.out.println(best + " + " + delta);
+									best += delta;
+//									if (cost(assignments) != best) {
+//										System.err.println("invalid best score!");
+//										System.exit(0);
+//									}
+									System.out.println(best);
+									improvement = true;
+									i = 0;
+									break; // start from family 0 again
+								}
+								if(delta > 0.0) {
+									break; // family was moved, move on to next family
+								}
+							}
 						}
 					}
 				}
 			}
-		} while(improvement);
+		} while (improvement);
 		return best;
 	}
 
-	private void sanity(int[] assignments) throws Exception {
-		int[] dayCaps = new int[100+1];
-		for(int i = 0; i < assignments.length; i++) {
+	private void sanity(int[] assignments) {
+		int[] dayCaps = new int[100 + 1];
+		for (int i = 0; i < assignments.length; i++) {
 			int assignment = assignments[i];
 			dayCaps[assignment] += familySize[i];
 		}
-		for(int i = 1; i < dayCaps.length; i++) {
+		for (int i = 1; i < dayCaps.length; i++) {
 			int cap = dayCaps[i];
-			if(cap < MIN_PPL) throw new Exception("cap " + cap + " < " + MIN_PPL);
-			if(cap > MAX_PPL) throw new Exception("cap " + cap + " > " + MAX_PPL);
+			if(cap != dayCapacities[i] ) {
+				throw new RuntimeException("day " + i + " cap inconsistency:\n" +
+						Arrays.toString(dayCapacities) + " !=\n" + Arrays.toString(dayCaps));
+			}
+			if (cap < MIN_PPL) {
+				throw new RuntimeException("cap " + cap + " < " + MIN_PPL);
+			}
+			if (cap > MAX_PPL) {
+				throw new RuntimeException("cap " + cap + " > " + MAX_PPL);
+			}
 		}
 	}
 
 	private double optimise(final int[] assignments) {
 		double best = localMinima(assignments, 0);
 		System.out.println("best = " + best);
-		for(int i = 0; i < 10000; i++) {
+		for (int i = 0; i < 10000; i++) {
 
 			localMinima(assignments, 0.00001);
 			double score = localMinima(assignments, 0);
 
-			if(score < best) {
+//			if(score != cost(assignments)) {
+//				System.err.println("invalid score!");
+//				System.exit(1);
+//			}
+
+			if (score < best) {
 				best = score;
-				try {
+
 					sanity(assignments);
 					System.out.println("**** new best = " + best + " ****");
 					CsvUtil.write(assignments, "../../solutions/" + score + ".csv");
 					CsvUtil.write(assignments, "../../solutions/best.csv");
-				} catch (Exception e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
+
 			}
 		}
 		return best;
@@ -189,19 +216,26 @@ public class Main {
 
 	public static void main(String[] meh) {
 		int[][] family_data = CsvUtil.read("../../../data/family_data.csv");
-		int[][] starting_solution = CsvUtil.read("../../../submission_71647.5625.csv");
-		//int[][] starting_solution = CsvUtil.read("/tmp/lala.csv"); // 77124.66595889143
+		//int[][] starting_solution = CsvUtil.read("../../../submission_71647.5625.csv");
+		int[][] starting_solution = CsvUtil.read("/tmp/lala.csv"); // 77124.66595889143
 		//int[][] starting_solution = CsvUtil.read("../../solutions/best.csv");
 		//int[][] starting_solution = CsvUtil.read("../../solutions/bad.csv"); //  1725999432440170
 
+		// 71757.52
+		//int[][] starting_solution = CsvUtil.read("../../solutions/71535.25513531327.csv");
+
 		assert starting_solution != null;
 		int[] initialAsignments = new int[starting_solution.length];
-		for(int i = 0; i < starting_solution.length; i++) {
+		for (int i = 0; i < starting_solution.length; i++) {
 			initialAsignments[i] = starting_solution[i][1];
 		}
 		Main main = new Main(family_data, initialAsignments);
-		//double score = main.localMinima(initialAsignments);
-		double score = main.optimise(initialAsignments);
-		System.out.println("minima = " + score);
+		//System.out.println(main.cost(initialAsignments));
+
+		double score = main.localMinima(initialAsignments, 0);
+		System.out.println(score);
+		//CsvUtil.write(initialAsignments, "/tmp/x.csv");
+		//double score = main.optimise(initialAsignments);
+		//System.out.println("minima = " + score);
 	}
 }
